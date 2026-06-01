afe248d30786827e13c25526a3ee124e5069ba2b
Jan Eitzinger <jan@moebiusband.org>
2026-06-01 09:57:16 +0200

    Fix GPU build config

--- mk/include_NVCC.mk
+++ mk/include_NVCC.mk
@@ -1,10 +1,5 @@
-CC = gcc
-
-# CUDA compiler
-NVCC = nvcc
-
-# Linker: use nvcc so it can resolve CUDA runtime symbols
-LD = $(NVCC)
+CC = nvcc
+LD = $(CC)
 
 ifeq ($(strip $(ENABLE_OPENMP)),true)
 OPENMP   = -Xcompiler "-fopenmp"
@@ -12,8 +7,8 @@ endif
 
 VERSION  = --version
 CFLAGS   = -O3 -std=c++17 $(CUDA_ARCH) $(OPENMP)
-LFLAGS   = $(OPENMP) -lm $(CUDA_ARCH)
+LFLAGS   = $(OPENMP)
 
 DEFINES  +=  -DENABLE_CUDA -D_GNU_SOURCE -DRUNTIME_BACKEND_IS_CUDA
 INCLUDES  =
-LIBS      = -lcudart -lm
+LIBS      = -lcudart
